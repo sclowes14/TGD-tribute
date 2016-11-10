82ed4abe501b4b6adba0ceb4106bf24299e26930
Stephen Clowes <sclowes14@gmail.com>
2016-11-10 16:47:38 -0500

    First commit

---
+++ js/home.js
@@ -0,0 +1,29 @@
+// My font sizes
+// ======================
+$('.tile .quote blockquote p.reg').flowtype({
+  minimum   : 0,
+  minFont   : 12,
+  maxFont   : 80,
+  fontRatio : 14
+});
+
+$('.tile .quote blockquote p.med-long').flowtype({
+  minimum   : 0,
+  minFont   : 10,
+  maxFont   : 80,
+  fontRatio : 16
+});
+
+$('.tile .quote blockquote p.long').flowtype({
+  minimum   : 0,
+  minFont   : 8,
+  maxFont   : 80,
+  fontRatio : 18
+});
+
+$('.tile .quote blockquote cite').flowtype({
+  minimum   : 0,
+  minFont   : 10,
+  maxFont   : 80,
+  fontRatio : 20
+});
